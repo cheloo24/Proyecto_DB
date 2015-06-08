dc06cc46cf7d7141824e605e9ed40397d8c8c7fd
polanco <diego.polancob@usach.cl>
2015-06-08 18:13:19 -0400

    base de datos con llaves foraneas

---
+++ cuicolog.sql
@@ -0,0 +1,198 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-06-2015 a las 04:07:42
+-- Versión del servidor: 5.6.20
+-- Versión de PHP: 5.5.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `cuicolog`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `calificar`(IN `id` INT, IN `puntuacion` INT)
+BEGIN
+IF puntuacion<6 THEN
+INSERT INTO calificacion VALUES(NULL,puntuacion);
+END IF;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `calificar2`(IN `puntuacion` INT)
+BEGIN
+IF puntuacion<6 THEN
+INSERT INTO calificacion VALUES(NULL,puntuacion);
+END IF;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+`id_admin` int(10) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `contraseña` varchar(24) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `calificacion`
+--
+
+CREATE TABLE IF NOT EXISTS `calificacion` (
+`id_calificacion` int(10) NOT NULL,
+  `id_foto` int(10) NOT NULL,
+  `puntuacion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentario`
+--
+
+CREATE TABLE IF NOT EXISTS `comentario` (
+`id_comentario` int(20) NOT NULL,
+  `id_foto` int(10) NOT NULL,
+  `fecha_comentario` datetime NOT NULL,
+  `caracteres_comentario` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `foto`
+--
+
+CREATE TABLE IF NOT EXISTS `foto` (
+`id_foto` int(10) NOT NULL,
+  `id_usuario` int(10) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `descripcion` varchar(200) NOT NULL,
+  `cant_comentarios` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+`id_usuario` int(10) NOT NULL,
+  `apodo_usuario` varchar(100) NOT NULL,
+  `mail_usuario` varchar(100) NOT NULL,
+  `contraseña` varchar(24) NOT NULL,
+  `nombre_usuario` varchar(24) NOT NULL,
+  `apellido_usuario` varchar(100) NOT NULL,
+  `edad_usuario` int(11) NOT NULL,
+  `cant_fotos` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `admin`
+--
+ALTER TABLE `admin`
+ ADD PRIMARY KEY (`nombre`), ADD UNIQUE KEY `id_admin` (`id_admin`), ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- Indices de la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+ ADD PRIMARY KEY (`id_foto`), ADD UNIQUE KEY `id_calificacion` (`id_calificacion`), ADD UNIQUE KEY `id_foto` (`id_foto`);
+
+--
+-- Indices de la tabla `comentario`
+--
+ALTER TABLE `comentario`
+ ADD PRIMARY KEY (`id_foto`), ADD UNIQUE KEY `id_comentario` (`id_comentario`), ADD UNIQUE KEY `id_foto` (`id_foto`);
+
+--
+-- Indices de la tabla `foto`
+--
+ALTER TABLE `foto`
+ ADD PRIMARY KEY (`id_usuario`), ADD UNIQUE KEY `id_foto` (`id_foto`), ADD UNIQUE KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+ ADD PRIMARY KEY (`id_usuario`), ADD UNIQUE KEY `id_usuario` (`id_usuario`), ADD UNIQUE KEY `apodo_usuario` (`apodo_usuario`), ADD UNIQUE KEY `mail_usuario` (`mail_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `admin`
+--
+ALTER TABLE `admin`
+MODIFY `id_admin` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+MODIFY `id_calificacion` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `comentario`
+--
+ALTER TABLE `comentario`
+MODIFY `id_comentario` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `foto`
+--
+ALTER TABLE `foto`
+MODIFY `id_foto` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+MODIFY `id_usuario` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+ADD CONSTRAINT `calificacion_ibfk_1` FOREIGN KEY (`id_foto`) REFERENCES `foto` (`id_foto`);
+
+--
+-- Filtros para la tabla `comentario`
+--
+ALTER TABLE `comentario`
+ADD CONSTRAINT `comentario_ibfk_1` FOREIGN KEY (`id_foto`) REFERENCES `foto` (`id_foto`);
+
+--
+-- Filtros para la tabla `foto`
+--
+ALTER TABLE `foto`
+ADD CONSTRAINT `foto_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
