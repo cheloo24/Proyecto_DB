a73bfaf3451afe4ba3afff862712e578f402983d
polanco <diego.polancob@usach.cl>
2015-06-18 20:24:03 -0400

    Borrando app antigua

--- fotolog/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-	get 'admin/loginadmin'
-	get 'admin/prueba'
-	get 'usuario/registro'
-	get 'usuario/perfil'
-	get 'usuario/loginuser'
-	root 'controlador#index'
-end
